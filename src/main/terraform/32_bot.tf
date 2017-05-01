4dfca5fb677314ae8e15cec4733217ce3c67ae90
Andrey Adamovich <andrey@aestasit.com>
2017-05-01 15:10:30 +0300

    fixed function name

--- src/main/terraform/32_bot.tf
+++ src/main/terraform/32_bot.tf
@@ -2,7 +2,7 @@
 resource "aws_lambda_function" "craftbot_function" {
   s3_bucket               = "${aws_s3_bucket.latcraft_code.bucket}"
   s3_key                  = "event-manager.zip"
-  function_name           = "craftbot"
+  function_name           = "craftbot_function"
   description             = "CraftBot"
   role                    = "${aws_iam_role.latcraft_lambda_executor.arn}"
   handler                 = "${var.lambda_code_package_prefix}.router.CraftBotCommands::${var.lambda_code_default_method}"
