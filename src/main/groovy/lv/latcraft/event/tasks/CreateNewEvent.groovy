7ef75256b8402f132fa68db45754e2b5cebfcb76
Andrey Adamovich <andrey@aestasit.com>
2017-06-28 12:12:00 +0300

    skeleton for create new event task

--- src/main/groovy/lv/latcraft/event/tasks/CreateNewEvent.groovy
+++ src/main/groovy/lv/latcraft/event/tasks/CreateNewEvent.groovy
@@ -1,13 +1,25 @@
 package lv.latcraft.event.tasks
 
 import com.amazonaws.services.lambda.runtime.Context
+import lv.latcraft.event.lambda.mock.InternalContext
 
 class CreateNewEvent extends BaseTask {
 
   @Override
-  Map<String, String> doExecute(Map<String, String> input, Context context) {
-    // TODO: implement
-    return null
+  Map<String, String> doExecute(Map<String, String> request, Context context) {
+    Map response = [:]
+    if (futureEvents.size() > 0) {
+      slack.send("I'm sorry, master, but it seems that there's already one event planned, so, I will not create another one!")
+    } else {
+
+      // TODO: create event from template
+
+    }
+    response
+  }
+
+  static void main(String[] args) {
+    new CreateNewEvent().execute([:], new InternalContext())
   }
 
 }
