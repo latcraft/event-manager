b09d7e2161474738b87a7580e73dd0d14fba2c69
Andrey Adamovich <andrey@aestasit.com>
2017-06-28 12:12:46 +0300

    skeleton for publish on lanyrd task

--- src/main/groovy/lv/latcraft/event/tasks/PublishEventOnLanyrd.groovy
+++ src/main/groovy/lv/latcraft/event/tasks/PublishEventOnLanyrd.groovy
@@ -2,14 +2,23 @@ package lv.latcraft.event.tasks
 
 import com.amazonaws.services.lambda.runtime.Context
 import groovy.util.logging.Log4j
+import lv.latcraft.event.lambda.mock.InternalContext
 
 @Log4j("logger")
 class PublishEventOnLanyrd extends BaseTask {
 
   @Override
-  Map<String, String> doExecute(Map<String, String> input, Context context) {
-    // TODO: implement
-    return null
+  Map<String, String> doExecute(Map<String, String> request, Context context) {
+    Map response = [:]
+    futureEvents.each { Map event ->
+
+
+    }
+    response
+  }
+
+  static void main(String[] args) {
+    new PublishEventOnLanyrd().execute([:], new InternalContext())
   }
 
 }
